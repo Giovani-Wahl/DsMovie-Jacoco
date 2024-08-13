17ddb1e26d91a9601e58e0256d45eb6751018b3a
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-13 14:16:55 -0300

    authenticatedShouldReturnUserEntityWhenUserExists

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,20 +1,59 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserDetailsFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
 public class UserServiceTests {
 
 	@InjectMocks
-	private UserService service;
+	private UserService userService;
+
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private CustomUserUtil customUserUtil;
+
+	private String existingUserNane,nonExistingUserNane;
+	private UserEntity user;
+	private List<UserDetailsProjection> userDetails;
+
+	@BeforeEach
+	void setUp() throws Exception{
+		existingUserNane = "maria@gmail.com";
+		nonExistingUserNane = "notuser@gmail.com";
+
+		user = UserFactory.createUserEntity();
+		userDetails = UserDetailsFactory.createCustomAdminUser(existingUserNane);
+
+		Mockito.when(userRepository.findByUsername(existingUserNane)).thenReturn(Optional.of(user));
+
+	}
+
 
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		Mockito.when(customUserUtil.getLoggedUsername()).thenReturn(existingUserNane);
+		UserEntity result = userService.authenticated();
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getUsername(),existingUserNane);
 	}
 
 	@Test
