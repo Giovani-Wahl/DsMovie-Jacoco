eccc973e83b0265c3ebf266fdefbd5bf51e0ae0e
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-22 16:46:48 -0300

    REST assured Tests MovieController and ScoreController, ok

---
+++ src/test/java/com/devsuperior/dsmovie/tests/TokenUtilRestAssured.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dsmovie.tests;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class TokenUtilRestAssured {
+
+    private static Response authRequest(String username,String password){
+        return given()
+                .auth()
+                .preemptive()
+                .basic("myclientid","myclientsecret")
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("username", username)
+                .formParam("password", password)
+                .formParam("grant_type", "password")
+                .when()
+                .post("/oauth2/token");
+    }
+
+    public static String obtainAccessToken(String username,String password){
+        Response response = authRequest(username, password);
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Failed to obtain access token: " + response.getStatusLine());
+        }
+        JsonPath jsonBody = response.jsonPath();
+        return jsonBody.getString("access_token");
+    }
+}
